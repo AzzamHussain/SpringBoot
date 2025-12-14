a1bab59a2d019f777e129bed8d151d2686f6b132
AzzamHussain <azzamhussain70@gmail.com>
2025-12-15 01:37:25 +0500

    learning api

--- demo/src/main/java/com/myproject/demo/teacher.java
+++ demo/src/main/java/com/myproject/demo/teacher.java
@@ -1,18 +1,32 @@
 package com.myproject.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+
+@RestController //webservices create karnay k liye hota hay
+@RequestMapping("/my")
 public class teacher {
 
-    @Autowired
-    student s1;
-    @GetMapping("/hello")
-    public String greetings() {
-        return s1.hello();
+    // @Autowired
+    // student s1;
+    // @GetMapping("/hello")
+    // public String greetings() {
+    //     return s1.hello();
+    // }
+    @GetMapping("/api1")
+    public String api1() {
+        return "I am your first api";
+    }
+     @GetMapping("/api2")
+    public String api2() {
+        return "I am your second api";
+    }
+     @GetMapping("/api3")
+    public String api3() {
+        return "I am your third api";
     }
     
 }
